233d7b8be3e66b42de9e9b16628d15e6971004e5
RapRy <bigbosstitle@gmail.com>
2020-06-14 06:42:27 -0500

    test error msg

--- reactSandbox/todolist/src/App.js
+++ reactSandbox/todolist/src/App.js
@@ -1,18 +1,53 @@
 import React from 'react';
 import axios from 'axios';
+import './style.css';
 
 import Instructions from './components/Instructions';
 import AddInput from './components/AddInput';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons'
+
 class App extends React.Component{
+    state = {
+        inputValue: "",
+        errorMsg: ""
+    }
+
+    getInputValue = () => {
+        const addItemInput = document.getElementById('addItemInput');
+        if(addItemInput.value == "" || addItemInput.value == null){
+            this.setState({errorMsg: "Field is empty!"})
+        }else if(addItemInput.value.length > 50){
+            this.setState({errorMsg: "Must be atleast 50 characters only!"})
+        }else{
+            this.setState({
+                inputValue: addItemInput.value, 
+                errorMsg: ""
+            })
+        }
+    }
+
+    testError = () => {
+        if(this.state.errorMsg != ""){
+            return( <div className="errorMsg">
+                        <p className="error">
+                            {<FontAwesomeIcon icon={faExclamationCircle} />}
+                            <span>{this.state.errorMsg}</span>
+                        </p>
+                    </div>)
+        }
+    }
+
     render(){
         return(
             <main className="mainContainer">
                 <section className="createContainer">
                     <div className="addListContainer">
                         <Instructions />
-                        <AddInput />
+                        <AddInput getValue={this.getInputValue}/>
                     </div>
+                    {this.testError()}
                 </section>
             </main>
         )
--- reactSandbox/todolist/src/components/AddInput.js
+++ reactSandbox/todolist/src/components/AddInput.js
@@ -4,11 +4,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
 class AddInput extends React.Component{
+
     render(){
         return(
             <form id="addForm">
                 <input type="text" name="newItem" placeholder="New Item..." id="addItemInput"/>
-                <button type="button" className="addItemBtn" id="addItemBtn" name="add">
+                <button type="button" className="addItemBtn" id="addItemBtn" name="add" onClick={this.props.getValue} >
                     <FontAwesomeIcon icon={faPlus} />
                     <span>Add</span>
                 </button>
